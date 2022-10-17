cf51a497e3a3bc42bdf64b504e761939a8c4d98f
MatthewThe <MatthewThe@users.noreply.github.com>
2022-10-17 12:14:34 +0200

    Try fixing UPX path again

--- gui_tools/build_gui.bat
+++ gui_tools/build_gui.bat
@@ -1,2 +1,2 @@
 :::: pyinstaller gui.py --noconfirm --onedir --name="PickedGroupFDR"
-pyinstaller --upx-dir ..\UPX\upx-3.96\ --noconfirm ../PickedGroupFDR.spec && python create_gui_zip.py
+pyinstaller --upx-dir ..\UPX\upx-3.96-win64\ --noconfirm ../PickedGroupFDR.spec && python create_gui_zip.py
