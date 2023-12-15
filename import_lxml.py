adea52973173c6aed843054ebf39440d7358c5ac
Matthew The <matthew.the@tum.de>
2023-12-15 21:19:13 +0100

    fix libxml2 version in GUI build

--- import_lxml.py
+++
@@ -1,7 +0,0 @@
-import PyInstaller.utils.hooks
-
-print("Trying to collect submodules for lxml")
-PyInstaller.utils.hooks.collect_submodules('lxml')
-print("Trying to collect submodules for lxml.isoschematron")
-PyInstaller.utils.hooks._collect_submodules('lxml.isoschematron', 'warn once')
-print("Success??")
