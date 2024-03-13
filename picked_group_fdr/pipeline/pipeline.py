cc101df641e2c592900991d3e536615619750573
Matthew The <matthew.the@tum.de>
2024-03-13 17:14:14 +0100

    add --suppress_missing_peptide_warning to pipeline methods

--- picked_group_fdr/pipeline/pipeline.py
+++ picked_group_fdr/pipeline/pipeline.py
@@ -26,6 +26,7 @@ def run_picked_group_fdr_all(
     do_quant: bool,
     lfq_min_peptide_ratios: int,
     fdr_cutoff: float,
+    suppress_missing_peptide_warning: bool = False,
 ):
     try:
         if len(output_dir) == 0:
@@ -42,7 +43,11 @@ def run_picked_group_fdr_all(
             if input_type == "mq":
                 pout_input_type = "andromeda"
                 pin_files = run_andromeda_to_pin(
-                    evidence_files, fasta_files, output_dir, digest_params_list
+                    evidence_files,
+                    fasta_files,
+                    output_dir,
+                    digest_params_list,
+                    suppress_missing_peptide_warning,
                 )
                 pout_files = run_mokapot(pin_files, output_dir)
 
@@ -50,7 +55,11 @@ def run_picked_group_fdr_all(
                 f"{output_dir}/evidence_{idx}.txt" for idx in range(len(evidence_files))
             ]
             run_update_evidence(
-                evidence_files, pout_files, evidence_files_rescored, pout_input_type
+                evidence_files,
+                pout_files,
+                evidence_files_rescored,
+                pout_input_type,
+                suppress_missing_peptide_warning,
             )
             run_picked_group_fdr(
                 evidence_files_rescored,
@@ -59,17 +68,27 @@ def run_picked_group_fdr_all(
                 digest_params_list,
                 do_quant,
                 lfq_min_peptide_ratios,
+                suppress_missing_peptide_warning,
             )
         elif input_type == "percolator_remap":  # currently not accessible by the GUI
             pout_merged = f"{output_dir}/pout_merged.txt"
             run_merge_pout_remap(
-                pout_files, fasta_files, output_dir, digest_params_list
+                pout_files,
+                fasta_files,
+                output_dir,
+                digest_params_list,
+                suppress_missing_peptide_warning,
             )
             run_picked_group_fdr_percolator_input_remap(
-                pout_merged, fasta_files, protein_groups_out
+                pout_merged,
+                fasta_files,
+                protein_groups_out,
+                suppress_missing_peptide_warning,
             )
         elif input_type == "percolator":
-            run_picked_group_fdr_percolator_input(pout_files, protein_groups_out)
+            run_picked_group_fdr_percolator_input(
+                pout_files, protein_groups_out, suppress_missing_peptide_warning
+            )
         else:
             logger.error(
                 f"Error while running Picked Group FDR, unknown input type: {input_type}."
@@ -92,6 +111,7 @@ def run_andromeda_to_pin(
     fasta_files: List[str],
     output_dir: str,
     digest_params_list: List[DigestionParams],
+    suppress_missing_peptide_warning: bool,
 ):
     pin_files = list()
     for idx, (evidence_file, digest_params) in enumerate(
@@ -103,6 +123,9 @@ def run_andromeda_to_pin(
             [evidence_file, "--outputTab", pin_file, "--databases"]
             + fasta_files
             + digest_params_str
+            + suppress_missing_peptide_warning_flag(
+                suppress_missing_peptide_warning,
+            )
         )
         pin_files.append(pin_file)
     return pin_files
@@ -130,6 +153,7 @@ def run_update_evidence(
     pout_files: List[str],
     evidence_files_rescored: List[str],
     pout_input_type: str,
+    suppress_missing_peptide_warning: bool,
 ):
     if len(evidence_files) != len(evidence_files_rescored):
         logger.error("Unequal number of input and output evidence files.")
@@ -146,6 +170,9 @@ def run_update_evidence(
                 "--pout_input_type",
                 pout_input_type,
             ]
+            + suppress_missing_peptide_warning_flag(
+                suppress_missing_peptide_warning,
+            )
         )
 
 
@@ -156,6 +183,7 @@ def run_picked_group_fdr(
     digest_params_list: List[DigestionParams],
     do_quant: bool,
     lfq_min_peptide_ratios: int,
+    suppress_missing_peptide_warning: bool,
 ):
     digest_params_str = digestion_params_list_to_arg_list(digest_params_list)
     quant_flags = []
@@ -165,6 +193,7 @@ def run_picked_group_fdr(
             "--lfq_min_peptide_ratios",
             str(lfq_min_peptide_ratios),
         ]
+
     picked_group_fdr.main(
         ["--mq_evidence"]
         + evidence_files
@@ -179,17 +208,24 @@ def run_picked_group_fdr(
         + fasta_files
         + digest_params_str
         + quant_flags
+        + suppress_missing_peptide_warning_flag(
+            suppress_missing_peptide_warning,
+        )
     )
 
 
 def run_merge_pout(
     pout_files: List[str],
     pout_merged: str,
+    suppress_missing_peptide_warning: bool,
 ):
     merge_pout.main(
         ["--perc_results"]
         + pout_files
         + ["--perc_merged", pout_merged]
+        + suppress_missing_peptide_warning_flag(
+            suppress_missing_peptide_warning,
+        )
     )
 
 
@@ -198,6 +234,7 @@ def run_merge_pout_remap(
     fasta_files: List[str],
     pout_merged: str,
     digest_params_list: List[DigestionParams],
+    suppress_missing_peptide_warning: bool,
 ):
     digest_params_str = digestion_params_list_to_arg_list(digest_params_list)
     merge_pout.main(
@@ -206,11 +243,16 @@ def run_merge_pout_remap(
         + ["--perc_merged", pout_merged, "--fasta"]
         + fasta_files
         + digest_params_str
+        + suppress_missing_peptide_warning_flag(
+            suppress_missing_peptide_warning,
+        )
     )
 
 
 def run_picked_group_fdr_percolator_input(
-    pout_files: List[str], protein_groups_out: str
+    pout_files: List[str],
+    protein_groups_out: str,
+    suppress_missing_peptide_warning: bool,
 ):
     picked_group_fdr.main(
         [
@@ -223,11 +265,17 @@ def run_picked_group_fdr_percolator_input(
             "--protein_groups_out",
             protein_groups_out,
         ]
+        + suppress_missing_peptide_warning_flag(
+            suppress_missing_peptide_warning,
+        )
     )
 
 
 def run_picked_group_fdr_percolator_input_remap(
-    pout_merged: str, fasta_files: List[str], protein_groups_out: str
+    pout_merged: str,
+    fasta_files: List[str],
+    protein_groups_out: str,
+    suppress_missing_peptide_warning: bool,
 ):
     picked_group_fdr.main(
         [
@@ -240,6 +288,9 @@ def run_picked_group_fdr_percolator_input_remap(
             "--fasta",
         ]
         + fasta_files
+        + suppress_missing_peptide_warning_flag(
+            suppress_missing_peptide_warning,
+        )
     )
 
 
@@ -256,3 +307,11 @@ def run_filter_fdr_maxquant(
             str(fdr_cutoff),
         ]
     )
+
+
+def suppress_missing_peptide_warning_flag(
+    suppress_missing_peptide_warning,
+) -> List[str]:
+    if suppress_missing_peptide_warning:
+        return ["--suppress_missing_peptide_warning"]
+    return []
