17c492542b9a8aabf74aa1115146d4e8017a4566
MatthewThe <MatthewThe@users.noreply.github.com>
2025-04-05 22:51:40 +0200

    Clean up create_gui
    pyinstaller no longer pollutes the root directory so we can skip moving libraries to subfolders

--- gui_tools/create_gui_zip.py
+++ gui_tools/create_gui_zip.py
@@ -3,18 +3,5 @@ import shutil
 
 dist_dir = Path.cwd() / 'dist' / 'PickedGroupFDR'
 
-lib_dir = dist_dir / 'lib'
-lib_dir.mkdir(parents=True, exist_ok=True)
-
-lib_bin_dir = dist_dir / 'Library' / 'bin'
-lib_bin_dir.mkdir(parents=True, exist_ok=True)
-
-print("Moving libraries to lib directory")
-for f in dist_dir.glob('*'):
-    if f.name.endswith(".egg-info'") or f.name in ["pytz", "matplotlib", "sqlalchemy", "tcl8", "PIL", "greenlet", "certifi"]:
-        shutil.rmtree(f)
-    elif f.is_file() and f.name not in ['base_library.zip', 'python36.dll', 'python37.dll', 'python38.dll', 'python39.dll', 'llvmlite.dll', 'PickedGroupFDR.exe', 'pyproject.toml']:
-        f.rename(lib_dir / f.name)
-
 print("Creating zip archive")
 shutil.make_archive(Path.cwd() / 'dist' / 'PickedGroupFDR_GUI_windows', 'zip', dist_dir)
