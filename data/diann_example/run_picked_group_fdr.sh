341dea9645b64c95d7665f29ae5c203369427288
Matthew The <matthew.the@tum.de>
2025-04-27 00:58:40 +0200

    add examples for other output formats with DIA-NN input

--- data/diann_example/run_picked_group_fdr.sh
+++ data/diann_example/run_picked_group_fdr.sh
@@ -6,3 +6,24 @@ python3 -u -m picked_group_fdr \
       --do_quant \
       --lfq_min_peptide_ratios 1 \
       --methods diann
+
+
+python3 -u -m picked_group_fdr \
+      --fasta ./diann/human_yeast_ecoli.fasta \
+      --fasta_use_uniprot_id \
+      --diann_reports ./diann/report.parquet \
+      --protein_groups_out ./results/combined_protein.tsv \
+      --output_format fragpipe \
+      --do_quant \
+      --lfq_min_peptide_ratios 1 \
+      --methods diann
+
+python3 -u -m picked_group_fdr \
+      --fasta ./diann/human_yeast_ecoli.fasta \
+      --fasta_use_uniprot_id \
+      --diann_reports ./diann/report.parquet \
+      --protein_groups_out ./results/proteinGroups.txt \
+      --output_format maxquant \
+      --do_quant \
+      --lfq_min_peptide_ratios 1 \
+      --methods diann
